bd667f89720fdadd88c9d44750101720764721b2
Rodrigo Simões <67639208+R0DR1235@users.noreply.github.com>
2022-04-25 14:00:15 +0100

    Delete src directory

--- src/pt/iscte/dcti/poo/sokoban/starter/ActiveObject.java
+++
@@ -1,9 +0,0 @@
-package pt.iscte.dcti.poo.sokoban.starter;
-
-import pt.iul.ista.poo.utils.Direction;
-
-public interface ActiveObject {
-	
-	boolean interact(AbstractObject a,Direction direction);
-    
-}
